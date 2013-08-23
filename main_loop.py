59ee7d9704cd8e192bd6acc0903e2c93fa7e9960
Nic Young <nryoung@gmail.com>
2013-08-23 01:09:01 -0600

    Initial commit. Scanner is working for a limited set of tokens.

---
+++ main_loop.py
@@ -0,0 +1,29 @@
+from scanner import Scanner
+import logging
+"""
+Main loop to test the Scanner
+"""
+
+micro_lang1 = """BEGIN -- SOMETHING UNUSUAL
+  READ(A1,New_A, D, B); 
+  C:= A1 +(New_A - D) - 75; 
+  New_C:=((B- (7)+(C+D)))-(3 - A1);-- STUPID FORMULA 
+  WRITE(C, A1+New_C); 
+  -- WHAT ABOUT := B+D;
+END"""
+
+token_list = []
+
+if __name__ == '__main__':
+    logging.basicConfig(filename='log.log', level=logging.DEBUG)
+
+    s = Scanner(micro_lang1)
+
+    while True:
+        token = s.scan()
+        logging.debug('Token: %s' % token)
+        if token:
+            token_list.append(token)
+        if token == 'EofSym':
+            break
+    print token_list
---
+++ scanner.py
@@ -0,0 +1,136 @@
+"""
+Scanner implementation in the Python
+"""
+import logging
+
+# Globals
+
+tokens = {'BEGIN': 'BeginSym',
+            'END': 'EndSym',
+            'READ': 'ReadSym',
+            'WRITE': 'WriteSym'
+           }
+
+
+class Scanner(object):
+
+    def __init__(self, micro_lang):
+        self.micro_lang = micro_lang
+        self.buffer = ''
+        self.i = iter(self.micro_lang)
+
+    def read(self):
+        return self.micro_lang[0]
+
+    def buffer_char(self, c):
+        self.buffer += c
+
+    def inspect(self, i):
+        try:
+            return self.micro_lang[i+1]
+        except IndexError:
+            return ''
+
+    def advance(self, i):
+        try:
+            self.micro_lang = self.micro_lang[i+1:]
+        except IndexError:
+            self.micro_lang = ''
+
+    def check_reserved(self):
+        logging.debug("Buffer: %s" % self.buffer)
+        if self.buffer in tokens:
+            return tokens[self.buffer]
+        else:
+            return 'Id'
+
+    def check_none(self, c):
+        return c == ' ' or \
+               c == '\t' or \
+               c == '\n'
+
+    def check_alpha(self, c, beg=None):
+        if beg:
+            return c.isalpha() and c.lower or \
+                   c.isalpha() and c.upper
+        else:
+            return c.isalpha() and c.lower or \
+                   c.isalpha() and c.upper or \
+                   c.isdigit() or c == '_'
+
+    def check_int_literal(self, c):
+        return c.isdigit()
+    def check_lparen(self, c):
+        return c == '('
+    def check_rparen(self, c):
+        return c == ')'
+    def check_scolon(self, c):
+        return c == ';'
+    def check_comma(self, c):
+        return c == ','
+    def check_plus(self, c):
+        return c == '+'
+
+    def scan(self):
+        # Clear the buffer
+        self.buffer = ''
+
+        # Check for EOF
+        if self.micro_lang == '':
+            return 'EofSym'
+
+        # Loop through our Micro Lang
+        else:
+            while True:
+                i = 0
+                char = self.read()
+                # Check if the char should be ignored
+                if self.check_none(char):
+                    self.advance(i)
+                    continue
+
+                # Check if alpha, if so grab th token
+                if self.check_alpha(char, beg=True):
+                    self.buffer_char(char)
+                    while True:
+                        next_char = self.inspect(i)
+                        if self.check_alpha(next_char):
+                            self.buffer_char(next_char)
+                            self.advance(i)
+
+                        else:
+                            self.advance(i)
+                            return self.check_reserved()
+
+                # Check for IntLiterals
+                if self.check_int_literal(char):
+                    self.buffer_char(char)
+                    while True:
+                        next_char = self.inspect(i)
+                        if self.check_int_literal(next_char):
+                            self.buffer_char(next_char)
+                            self.advance(i)
+                        else:
+                            self.advance(i)
+                            logging.debug('Buffer: %s' % self.buffer)
+                            return 'IntLiteral'
+
+                # Check for misc. chars
+                if self.check_lparen(char):
+                    self.advance(i)
+                    return 'LParen'
+                if self.check_rparen(char):
+                    self.advance(i)
+                    return 'RParen'
+                if self.check_scolon(char):
+                    self.advance(i)
+                    return 'SemiColon'
+                if self.check_comma(char):
+                    self.advance(i)
+                    return 'Comma'
+                if self.check_plus(char):
+                    self.advance(i)
+                    return 'PlusOp'
+                else:
+                    self.advance(i)
+                    return
