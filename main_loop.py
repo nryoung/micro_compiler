f5ba3ca97110e365727634928fce318bfeb7990a
Nic Young <nryoung@gmail.com>
2013-08-25 00:17:50 -0600

    Added try/except block for testing. Added second microlang to test. Cleaned up some comments.

--- main_loop.py
+++ main_loop.py
@@ -1,9 +1,11 @@
+"""
+Test runner for Scanner implementation
+"""
 from scanner import Scanner
+from scanner_errors import LexicalError
 import logging
-"""
-Main loop to test the Scanner
-"""
 
+# Globals to be used in testing
 micro_lang1 = """BEGIN -- SOMETHING UNUSUAL
   READ(A1,New_A, D, B); 
   C:= A1 +(New_A - D) - 75; 
@@ -12,19 +14,28 @@ micro_lang1 = """BEGIN -- SOMETHING UNUSUAL
   -- WHAT ABOUT := B+D;
 END"""
 
+micro_lang2 = """BEGIN 
+READ(A, B, C, D); 
+E := (A + B) - (C + D); -- Calculate
+WRITE(E); 
+END"""
+
 token_list = []
 
 if __name__ == '__main__':
     logging.basicConfig(filename='log.log', level=logging.DEBUG)
 
-    s = Scanner(micro_lang1)
+    s = Scanner(micro_lang2)
 
-    while True:
-        token = s.scan()
-        logging.debug('Token: %s' % token)
-        if token:
-            token_list.append(token)
-        if token == 'EofSym':
-            break
-
-    print token_list
+    try:
+        while True:
+            token = s.scan()
+            logging.debug('Token: %s' % token)
+            if token:
+                token_list.append(token)
+            if token == 'EofSym':
+                break
+    except LexicalError as e:
+        print "Lexical Error. Unknown character: '%s'" % e.bad_char
+    else:
+        print token_list
