cc666c9c544ef68bb66e24404deeed7522fb7bf8
Nic Young <nryoung@gmail.com>
2013-09-03 21:25:39 -0600

    Added CompilerError base class. Also added SyntaxError exception.

--- compiler_errors.py
+++ compiler_errors.py
@@ -5,9 +5,23 @@ Course: CSCI 4640
 Assignment #1
 """
 
-class LexicalError(Exception):
+class CompilerError(Exception):
+    """
+    Base class for exceptions in this compiler module.
+    """
+    pass
+
+class LexicalError(CompilerError):
     """
     Exception raised when a lexical error is detected.
+    """
+    def __init__(self, bad_char):
+        self.bad_char = bad_char
+
+
+class SyntaxError(CompilerError):
+    """
+    Exception raised when a syntax error is detected.
     """
     def __init__(self, bad_char):
         self.bad_char = bad_char
