6a87e1ff970695e4aefb39fa32618df93fd1e63c
Nic Young <nryoung@gmail.com>
2013-09-13 21:41:32 -0600

    Added ExprRec data structure and associated exceptions.

--- compiler/compiler_errors.py
+++ compiler/compiler_errors.py
@@ -25,9 +25,23 @@ class SyntaxError(CompilerError):
     def __init__(self, syn_err):
         self.err = syn_err
 
-class TypeError(CompilerError):
+class OpError(CompilerError):
     """
-    Exception raised when a type error is detected.
+    Exception raised when a invalid operater is detected.
     """
     def __init__(self, typ_error):
         self.err = typ_error
+
+class LiteralError(CompilerError):
+    """
+    Exception raised when a invalid Id is detected.
+    """
+    def __init__(self, lit_error):
+        self.err = lit_error
+
+class IdError(CompilerError):
+    """
+    Exception raised when a invalid Id is detected.
+    """
+    def __init__(self, id_error):
+        self.err = id_error
--- compiler/data_structures.py
+++ compiler/data_structures.py
@@ -2,7 +2,9 @@
 Data structures to be used in the micro micro compiler.
 """
 
-from compiler_errors import TypeError
+from compiler_errors import OpError
+from compiler_errors import LiteralError
+from compiler_errors import IdError
 
 class OpRec(object):
 
@@ -18,6 +20,40 @@ class OpRec(object):
     @op.setter
     def op(self, o):
         if o not in self.allowed_vals:
-            raise TypeError("Operator must either be a '+' or '-'")
+            raise OpError("Operator must either be a '+' or '-'")
         else:
             self._op = o
+
+class ExprRec(object):
+
+    def __init__(self, expr, literal=None):
+        if literal:
+            self.val = expr
+        else:
+            self.name = expr
+
+    @property
+    def val(self):
+        return self._val
+
+    @val.setter
+    def val(self, expr):
+        try:
+            v = int(expr)
+        except ValueError:
+            raise LiteralError("Invalid invalid literal with base 10: '%s'" % expr)
+        else:
+            self._val = v
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, expr):
+        try:
+            n = int(expr)
+        except ValueError:
+            self._name = expr
+        else:
+            raise IdError("Invalid Id name: '%s'" % expr)
