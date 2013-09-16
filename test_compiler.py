e3640a3ffbe71542af08ccc0e3099f436cda3425
Nic Young <nryoung@gmail.com>
2013-09-16 02:11:12 -0600

    Added test compile option along with appropriate in/out file options. * Added WriteObj that collects all messages to stdout to later be written to a file specified by the user

--- test_compiler.py
+++ test_compiler.py
@@ -1,13 +1,37 @@
 """
 Test runner for Compiler implementation.
-Depending on cmd line arguments it will run specifica tests.
+Depending on cmd line arguments it will run specific tests.
 Name: Nicholas Young
 Course: CSCI 4640
 """
 import argparse
 import tests
 import unittest
+import traceback
+import sys
 from compiler.compiler_errors import SyntaxError, LexicalError
+
+# Small class that collects all sys_stdout which
+# allows it to be later written to a file.
+class WriteObj(object):
+    def __init__(self):
+        self.content =""
+    def write(self, s):
+        self.content += s
+
+def test_compile(in_file, out_file):
+    from compiler.parser import Parser
+    p = Parser(in_file)
+    try:
+        output = WriteObj()
+        sys.stdout = output
+        p.system_goal()
+    except Exception as e:
+        print traceback.format_exc()
+    else:
+        sys.stdout = sys.__stdout__
+        out_file.write(output.content)
+        print "Compiled to location: %s" % out_file.name
 
 def test_parser(program):
     from compiler.parser import Parser
@@ -53,18 +77,24 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('test_type', type=str,
                         help="""The component of the compiler to be tested.
-                             Options are: 'scanner' | 'parser' | 'unit'""")
-    parser.add_argument('--file', type=argparse.FileType('r'),
-                        help='Name of the file to be tested with.')
+                             Options are: 'scanner' | 'parser' | 'unit' | 'compile'""")
+    parser.add_argument('--in_file', type=argparse.FileType('r'),
+                        help='Name of the input file.')
+
+    parser.add_argument('--out_file', default='output/op_code',
+                        type=argparse.FileType('w'),
+                        help='Name of the output file.')
 
     args = parser.parse_args()
 
     # Run the specific test depending on the args passed in
     if args.test_type == 'scanner':
-        test_scanner(args.file)
+        test_scanner(args.in_file)
     elif args.test_type == 'parser':
-        test_parser(args.file)
+        test_parser(args.in_file)
     elif args.test_type == 'unit':
         unit_tests()
+    elif args.test_type == 'compile':
+        test_compile(args.in_file, args.out_file)
     else:
         parser.print_help()
